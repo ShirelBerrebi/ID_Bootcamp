2bb2153b24065d62c12816bdb3d78b71ad747d99
ShirelBerrebi <s0583264738@gmail.com>
2022-01-19 17:22:53 +0200

    update

---
+++ week3/Day2/main.js
@@ -0,0 +1,6 @@
+
+let favoritefood = "milk" 
+
+let favoritemeal = "bbreakfast"
+
+console.log ("I eat <favoritefood> at every <favoritemeal>")
---
+++ week3/Day4/xp.js
@@ -0,0 +1,9 @@
+let x= 5
+let y= 9
+
+if (x>y) {
+    console.console.log(x);
+} else {
+    console.console.log(y);
+}
+
